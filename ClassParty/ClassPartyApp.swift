a3965b3af83c65855d2c0036c55a7e970b11abe8
zephyrwala <mayur@redphoenix.co.in>
2020-12-11 07:15:51 +0530

    Initial Commit

---
+++ ClassParty/ClassPartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassPartyApp.swift
+//  ClassParty
+//
+//  Created by Red Phoenix on 11/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ClassPartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
